9f07dab9684eb5e74a6c34b38257f75ceed8edc7
VIJAY-PRASATH-R <vijayprasath6371@gmail.com>
2025-12-14 13:43:00 +0530

    perf: Cache expensive computations

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+   id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128)) # Changed from password to password_hash
     name = db.Column(db.String(100), nullable=False)
